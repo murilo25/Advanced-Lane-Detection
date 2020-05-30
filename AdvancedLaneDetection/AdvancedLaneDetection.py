73e67d9a131fba5b2451c74025d37dd88edf9a46
Murilo Pinheiro <muriloaugusto25@hotmail.com>
2020-05-30 15:41:27 -0500

    Add sliding window to detect lane pixels

--- AdvancedLaneDetection/AdvancedLaneDetection.py
+++ AdvancedLaneDetection/AdvancedLaneDetection.py
@@ -104,7 +104,111 @@ def changePerspective(img):
 
     return birdsEye
 
-def drawLines(binary_birdsEdye):
+def findPixels(img):
+    ### find lane pixels coordinates using a sliding window approach
+    ### each line (left/right) has its own window that slides from the bottom to the top of the img
+    ### returns pixels coordinates
+    
+    img_size = (img.shape[1],img.shape[0])
+    # compute histogram to roughly estimate the lines x coordinates
+    histogram = []
+    for i in range (0,img_size[0]):
+        histogram.append(np.sum(img[:,i]))
+    #hist2=np.sum(img,axis = 0) # same as for loop above
+    plt.title("Histogram on x-axis")
+    plt.plot(histogram)
+    plt.show()
+
+    # estimates x coordinate of lines
+    left_window_center = np.argmax(histogram[:img_size[0]//2])
+    right_window_center = np.argmax(histogram[img_size[0]//2:]) + img_size[0]//2 
+
+    # define window parameters
+    n_windows = 9
+    window_height = np.int(img_size[1]//n_windows)
+    window_width = 100
+    min_pix = 50    # minimum number of pixels to recompute window_center
+
+    # create an empty array to store white pixels coordinates
+    left_line_coordinates = []
+    right_line_coordinates = []
+
+    # compute initial window coordinates
+    window_left_x_low_current = left_window_center - window_width//2
+    window_left_x_high_current = left_window_center + window_width//2
+    window_right_x_low_current = right_window_center - window_width//2
+    window_right_x_high_current = right_window_center + window_width//2
+
+    # identify all white pixels in the img
+    pixel_coordinates = img.nonzero() # returns a 2xN array that contains the coordinates of white pixels (N = # of white pixels in the image)
+    pixel_x = np.array(pixel_coordinates[1])
+    pixel_y = np.array(pixel_coordinates[0])
+
+    img_out = np.dstack((img,img,img))
+
+    # for each window, extract white pixels coordinates
+    for i in range (0,n_windows):
+        # compute i-th window coordinates
+        window_left_x_low = window_left_x_low_current
+        window_left_x_high = window_left_x_high_current
+        window_right_x_low = window_right_x_low_current
+        window_right_x_high = window_right_x_high_current
+        window_bottom = img_size[1] - window_height*(i)
+        window_top = img_size[1] - window_height*(i+1)
+
+        # Identify the nonzero pixels in x and y within the window (<val>.nonzero()[0] ??)
+        window_left_coordinates = ((pixel_y >= window_top) & (pixel_y < window_bottom) & 
+        (pixel_x >= window_left_x_low) &  (pixel_x < window_left_x_high)).nonzero()[0]
+        window_right_coordinates = ((pixel_y >= window_top) & (pixel_y < window_bottom) & 
+        (pixel_x >= window_right_x_low) &  (pixel_x < window_right_x_high)).nonzero()[0]
+        
+        # Append these indices to the lists
+        left_line_coordinates.append(window_left_coordinates)
+        right_line_coordinates.append(window_right_coordinates)
+
+        # If found > min_pix pixels, recenter next window on their mean position
+        if len(window_left_coordinates) > min_pix:
+            left_window_center = np.int(np.mean(pixel_x[window_left_coordinates]))
+            window_left_x_low_current = left_window_center - window_width//2
+            window_left_x_high_current = left_window_center + window_width//2
+        if len(window_right_coordinates) > min_pix:   
+            right_window_center = np.int(np.mean(pixel_x[window_right_coordinates]))
+            window_right_x_low_current = right_window_center - window_width//2
+            window_right_x_high_current = right_window_center + window_width//2
+
+        # Draw the windows on the visualization image
+        cv2.rectangle(img_out,(window_left_x_high,window_bottom),(window_left_x_low,window_top),(255,255,255), 10)
+        cv2.rectangle(img_out,(window_right_x_high,window_bottom),(window_right_x_low,window_top),(255,255,255), 10) 
+        #plt.imshow(img_out)
+        #plt.show()
+
+        #end for
+    #plt.show()
+    # Concatenate the arrays of indices (previously was a list of lists of pixels)
+    try:
+        left_line_coordinates = np.concatenate(left_line_coordinates)
+        right_line_coordinates = np.concatenate(right_line_coordinates)
+    except ValueError:
+        # Avoids an error if the above is not implemented fully
+        pass
+    # Extract left and right line pixel positions
+    left_x = pixel_x[left_line_coordinates]
+    left_y = pixel_y[left_line_coordinates] 
+    right_x = pixel_x[right_line_coordinates]
+    right_y = pixel_y[right_line_coordinates]
+
+    ## Visualization ##
+    # Colors pixels detected within windows
+    img_out[left_y, left_x] = [255, 0, 0]
+    img_out[right_y, right_x] = [0, 0, 255]
+    plt.imshow(img_out)
+    plt.show()
+
+    return left_x,left_y,right_x,right_y,img_out
+
+def drawLines(img):
+    left_x,left_y,right_x,right_y,img_out = findPixels(img)
+
     return 0
 
 # set grid size internal to the chess board
@@ -168,13 +272,13 @@ dst = cv2.undistort(img, mtx, dist, None, mtx)
 
 ### lane detection ###
 image = cv2.imread('test_images/straight_lines1.jpg')
-#image = cv2.imread('test_images/straight_lines2.jpg')
-#image = cv2.imread('test_images/test1.jpg')
-#image = cv2.imread('test_images/test2.jpg')
-#image = cv2.imread('test_images/test3.jpg')
-#image = cv2.imread('test_images/test4.jpg') # good but can improve
-#image = cv2.imread('test_images/test5.jpg') # bad
-#image = cv2.imread('test_images/test6.jpg')
+image = cv2.imread('test_images/straight_lines2.jpg')
+image = cv2.imread('test_images/test1.jpg')
+image = cv2.imread('test_images/test2.jpg')
+image = cv2.imread('test_images/test3.jpg')
+image = cv2.imread('test_images/test4.jpg') # good but can improve
+image = cv2.imread('test_images/test5.jpg') # bad
+image = cv2.imread('test_images/test6.jpg')
 undistorted_image = cv2.undistort(image, mtx, dist, None, mtx)
 birdsEye = changePerspective(undistorted_image) # apply region of interest mask and change perspective
 binary_birdsEye = detectLane(birdsEye) # detect all edges using sobel x absolute & color
